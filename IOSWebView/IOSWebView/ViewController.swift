f0b001edc1d55d634bf7f1994982a4aa40787f30
E S <es@Es-MacBook-Pro.local>
2019-08-12 11:26:10 +0300

    dev
    desc

--- IOSWebView/IOSWebView/ViewController.swift
+++ IOSWebView/IOSWebView/ViewController.swift
@@ -1,61 +1,36 @@
+//
+//  ViewController.swift
+//  IOSWebViewTutorial
+//
 
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
-    
+class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
-    var eventFunctions : Dictionary<String, (String)->Void> = Dictionary<String, (String)->Void>()
-    let events: Array<String> = ["finishCCreditCard", "documentReady", "callbackHandler"]
     
-    private func setupWebView() {
-        let contentController = WKUserContentController()
-        let userScript = WKUserScript(
-            source: "mobileHeader()",
-            injectionTime: WKUserScriptInjectionTime.atDocumentEnd,
-            forMainFrameOnly: true
-        )
-        contentController.addUserScript(userScript)
-        contentController.add(self, name: "callbackHandler")
-        
-        let config = WKWebViewConfiguration()
-        config.userContentController = contentController
-        webView = WKWebView(frame: view.bounds, configuration: config)
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("WebViewController load")
-        
-        self.setupWebView()
-        view.addSubview(webView)
-        
-        let url = URL(string: "http://localhost:3080/")!
+        // 1
+        let url = URL(string: "https://ioscreator.com")!
         webView.load(URLRequest(url: url))
         
+        // 2
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         toolbarItems = [refresh]
         navigationController?.isToolbarHidden = false
     }
     
-    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
 
-    
-    
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        print("userContentController")
-        if let contentBody = message.body as? String{
-            if let eventFunction = eventFunctions[message.name]{
-                eventFunction(contentBody)
-            }
-        }
-        
-        if(message.name == "callbackHandler") {
-            print("Launch my Native Camera")
-        }
-    }
+
 }
